3934dfeb681ffead43da802fc46c24182c8fc6df
lufergida <lufergida@gmail.com>
2023-03-31 19:32:05 -0500

    Second class

---
+++ dino_runner/components/cactus.py
@@ -0,0 +1,11 @@
+import random
+from dino_runner.components.obstacle import Obstacle
+
+
+class Cactus(Obstacle):
+# Image es una lista
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+    
